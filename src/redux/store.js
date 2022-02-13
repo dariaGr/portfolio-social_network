6ed2e1fb74b179c4890bea33b88b21e7df9b5b93
dariaGr <grb747@inbox.ru>
2022-02-13 09:05:08 +0100

    moving types to constants

--- src/redux/store.js
+++ src/redux/store.js
@@ -70,12 +70,8 @@ const store = {
     }
 };
 
-export const addPostActionCreator = () => {
-     return {type: ADD_POST};
-};
+export const addPostActionCreator = () => ({type: ADD_POST});
 
-export const updateNewPostTextActionCreator = (text) => {
-     return {type: UPDATE_NEW_POST_TEXT, newText: text};
-};
+export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text});
 
 export default store;
