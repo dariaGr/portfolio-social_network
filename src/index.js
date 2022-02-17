13519c441260f46afb5ff235184aab8ff7ab847b
dariaGr <grb747@inbox.ru>
2022-02-17 12:33:55 +0100

    Context

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import store from './redux/redux-store';
+import StoreContext from './StoreContext';
 
 const rerenderEntireTree = () => {
     ReactDOM.render(
        <React.StrictMode>
-         <App store={store}/>
+         <StoreContext.Provider value={store}>
+            <App /> 
+         </StoreContext.Provider>
        </React.StrictMode>,
        document.getElementById('root')
      );
