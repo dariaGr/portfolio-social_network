56a5ed3bbd03799b221f6ab6da3d334dfae710b5
dariaGr <grb747@inbox.ru>
2022-02-19 21:59:20 +0100

    axios get request

--- src/components/Main/FindUsers/FindUsers.jsx
+++ src/components/Main/FindUsers/FindUsers.jsx
@@ -1,40 +1,14 @@
 import s from './FindUsers.module.css';
+import axios from 'axios';
+import User from '../../../assets/user.jpg';
 
 const FindUsers = ({users, setUsers, toggleFollow}) => {
     if (users.length === 0) {
-        setUsers([
-            {id: 1, 
-            photoUrl: 'https://static.livebooks.com/a252d92208c3401c9af74171e154fbe1/i/d0dceb6b20d643afbb539ce2f02d7f47/1/GCuCv726vxAVM9sgguVj4g/7_0_172_1duhamelfinal.jpg',
-            followed: false,
-            fullName: 'Dmitry K.', 
-            status: 'I am looking for a job right now...',
-            location: {
-                city: 'Berlin',
-                country: 'Germany'
-                }
-            },
-            {id: 2, 
-            photoUrl: 'https://4.bp.blogspot.com/-PVm5fMH_j6Q/T7CqSHCJXHI/AAAAAAAAMr4/K2dTrioUmYc/s1600/dunn01.jpg',
-            followed: true,
-            fullName: 'Elena M.', 
-            status: 'Random status!',
-            location: {
-                city: 'Munich',
-                country: 'Germany'
-                }
-            },
-            {id: 3, 
-            photoUrl: 'https://th.bing.com/th/id/R.c6b482f5e343903955234a1f60e9ecda?rik=m789TZQTh%2fzXPA&riu=http%3a%2f%2fsunrift.com%2fwp-content%2fuploads%2f2014%2f12%2fBlake-profile-photo-square.jpg&ehk=KPaRRdhK2539xSF1ZlhKY25YNeohbiAO9FGYmnQIC7U%3d&risl=&pid=ImgRaw&r=0',
-            followed: false,
-            fullName: 'Michael T.', 
-            status: 'Sleeping...',
-            location: {
-                city: 'Dusseldorf',
-                country: 'Germany'
-                }
-            }
-        ]);
-    }
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {
+            setUsers(response.data.items)
+        })
+    };
 
     return (
         <div className={s.findUsers}>
@@ -43,19 +17,19 @@ const FindUsers = ({users, setUsers, toggleFollow}) => {
                 {users.map(user => 
                     <div key={user.id} className={s.userCard}>
                     <div className={s.column}>
-                        <img className={s.userImg} src={user.photoUrl} alt="user-avatar" />
+                        <img className={s.userImg} src={user.photos.small != null ? user.photos.small : User} alt="user-avatar" />
                         <button onClick={()=>{toggleFollow(user.id)}} className={s.followButton}>
                             {user.followed ? 'Unfollow' : 'Follow'}
                         </button>
                     </div>
                     <div className={s.userContent}>
                         <div className={s.description}>
-                            <div className={s.userName}>{user.fullName}</div>
+                            <div className={s.userName}>{user.name}</div>
                             <p className={s.userDesc}>{user.status}</p>
                         </div>
                         <div>
-                            <div className={s.userCity}>{user.location.city},</div>
-                            <div className={s.userCountry}>{user.location.country}</div>
+                            <div className={s.userCity}>{'user.location.city'},</div>
+                            <div className={s.userCountry}>{'user.location.country'}</div>
                         </div>
                     </div>
                 </div>
@@ -67,3 +41,36 @@ const FindUsers = ({users, setUsers, toggleFollow}) => {
 };
 
 export default FindUsers;
+
+// setUsers([
+        //     {id: 1, 
+        //     photoUrl: 'https://static.livebooks.com/a252d92208c3401c9af74171e154fbe1/i/d0dceb6b20d643afbb539ce2f02d7f47/1/GCuCv726vxAVM9sgguVj4g/7_0_172_1duhamelfinal.jpg',
+        //     followed: false,
+        //     fullName: 'Dmitry K.', 
+        //     status: 'I am looking for a job right now...',
+        //     location: {
+        //         city: 'Berlin',
+        //         country: 'Germany'
+        //         }
+        //     },
+        //     {id: 2, 
+        //     photoUrl: 'https://4.bp.blogspot.com/-PVm5fMH_j6Q/T7CqSHCJXHI/AAAAAAAAMr4/K2dTrioUmYc/s1600/dunn01.jpg',
+        //     followed: true,
+        //     fullName: 'Elena M.', 
+        //     status: 'Random status!',
+        //     location: {
+        //         city: 'Munich',
+        //         country: 'Germany'
+        //         }
+        //     },
+        //     {id: 3, 
+        //     photoUrl: 'https://th.bing.com/th/id/R.c6b482f5e343903955234a1f60e9ecda?rik=m789TZQTh%2fzXPA&riu=http%3a%2f%2fsunrift.com%2fwp-content%2fuploads%2f2014%2f12%2fBlake-profile-photo-square.jpg&ehk=KPaRRdhK2539xSF1ZlhKY25YNeohbiAO9FGYmnQIC7U%3d&risl=&pid=ImgRaw&r=0',
+        //     followed: false,
+        //     fullName: 'Michael T.', 
+        //     status: 'Sleeping...',
+        //     location: {
+        //         city: 'Dusseldorf',
+        //         country: 'Germany'
+        //         }
+        //     }
+        // ]);
