f986343adcc0ddb7d66320aad788fd7903de796a
dariaGr <grb747@inbox.ru>
2022-02-20 20:04:54 +0100

    introducing componentDidMount

--- src/components/Main/FindUsers/FindUsers.jsx
+++ src/components/Main/FindUsers/FindUsers.jsx
@@ -4,9 +4,7 @@ import axios from 'axios';
 import User from '../../../assets/user.jpg';
 
 class FindUsers extends React.Component {
-    constructor(props){
-        super(props)
-
+    componentDidMount(){
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items)
