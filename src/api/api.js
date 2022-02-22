78e2e3cc4ef92bb44c4fc63870b09bdef54ccf83
dariaGr <grb747@inbox.ru>
2022-02-22 12:58:39 +0100

    axios create instance

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
     headers: {'API-KEY': '7f367b50-e1e3-4664-b642-e4301e6d3072'}
-})
+});
 
 export const getUsers = (currentPage = 1, pageSize = 10) => {
     return (
