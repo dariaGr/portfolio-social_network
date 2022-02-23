fb4ed45d888586c5f66fec7b6d11cfcc0289bcaf
Daria Grebenjuk <49517845+dariaGr@users.noreply.github.com>
2022-02-23 11:37:00 +0100

    hoc withAuthRedirect

--- src/components/Main/Profile/ProfileContainer.jsx
+++ src/components/Main/Profile/ProfileContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux'; 
 import { useMatch } from 'react-router-dom';
 import s from './Profile.module.css';
@@ -6,39 +6,36 @@ import ProfileBg from './../../../assets/profile-bg.jpg';
 import PostsContainer from './Posts/PostsContainer';
 import ProfileBio from './ProfileBio/ProfileBio';
 import { getUserProfile } from './../../../redux/profileReducer';
+import { withAuthRedirect } from './../../../hoc/withAuthRedirect';
 
-class ProfileContainer extends React.Component {
+const ProfileContainer = (props) => {
+    const match = useMatch('/profile/:userId/');
+    let userId = match ? match.params.userId : '22550';
 
-    componentDidMount() {
-        let userId = this.props.match ? this.props.match.params.userId : '22550';
-        this.props.getUserProfile(userId);
-    };
-  
-    render() {
-        return (
-            <div className={s.profile}>
-                <div className={s.background}>
-                    <img src={ProfileBg} />
-                </div>
-                <div className={s.content}>
-                    <ProfileBio {...this.props} profile={this.props.profile} />
-                    <PostsContainer />
-                </div>
+    useEffect(() => {
+        props.getUserProfile(userId);
+    }, [])
+
+    return (
+        <div className={s.profile}>
+            <div className={s.background}>
+                <img src={ProfileBg} />
             </div>
-        );
-    };
+            <div className={s.content}>
+                <ProfileBio {...props} profile={props.profile} />
+                <PostsContainer />
+            </div>
+        </div>
+    );
 };
+
+let AuthRedirectComponent = withAuthRedirect(ProfileContainer);
 
 const mapStateToProps = state => {
     return {
-        profile: state.profilePage.profile
-    }
-}
+        profile: state.profilePage.profile,
+    };
+};
 
-const ProfileURLMatch = (props) => {
-    const match = useMatch('/profile/:userId/');
-    return <ProfileContainer {...props} match={match} />;
-}
-
-export default connect(mapStateToProps, {getUserProfile})(ProfileURLMatch);
+export default connect(mapStateToProps, {getUserProfile})(AuthRedirectComponent);
 
