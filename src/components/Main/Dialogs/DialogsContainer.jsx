3e47762e009252ebeb3ff75fd818f585499a2569
Daria Grebenjuk <49517845+dariaGr@users.noreply.github.com>
2022-02-23 11:36:01 +0100

    hoc withAuthRedirect

--- src/components/Main/Dialogs/DialogsContainer.jsx
+++ src/components/Main/Dialogs/DialogsContainer.jsx
@@ -1,15 +1,18 @@
 import { addMessage, updateNewMessageText } from '../../../redux/dialogsReducer';
 import { connect } from 'react-redux';
 import Dialogs from './Dialogs';
+import { withAuthRedirect } from './../../../hoc/withAuthRedirect';
 
-const mapStateToProps = (state) => {
+let AuthRedirectComponent = withAuthRedirect(Dialogs);
+
+const mapStateToProps = state => {
     return {
         newMessageText: state.dialogsPage.newMessageText,
         dialogs: state.dialogsPage.dialogs,
-        messages: state.dialogsPage.messages   
+        messages: state.dialogsPage.messages,
     };
 };
 
-const DialogsContainer = connect(mapStateToProps, {addMessage, updateNewMessageText})(Dialogs);
+const DialogsContainer = connect(mapStateToProps, {addMessage, updateNewMessageText})(AuthRedirectComponent);
 
 export default DialogsContainer;
