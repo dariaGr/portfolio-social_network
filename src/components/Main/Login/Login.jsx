d8a0d0f5a87940f7ce174b458f207513f71ffabd
dariaGr <grb747@inbox.ru>
2022-02-27 09:59:47 +0100

    Login

--- src/components/Main/Login/Login.jsx
+++ src/components/Main/Login/Login.jsx
@@ -1,6 +1,23 @@
+import s from './Login.module.css';
+
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <div className={s.formLoginContainer}>
+            <form className={s.formLogin}>
+                <div>
+                    <input type="text" placeholder="Login" />
+                </div>
+                <div>
+                    <input type="password" placeholder="Password" />
+                </div>
+                <div>
+                    <input type="checkbox" />Remember me
+                </div>
+                <div>
+                    <button>Log in</button>
+                </div>
+            </form>
+        </div>  
     );
 };
 
