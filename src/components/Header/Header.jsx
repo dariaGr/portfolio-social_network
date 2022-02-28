cb724281df1ad4d178a676e3625325cff3e32973
dariaGr <grb747@inbox.ru>
2022-02-28 04:12:42 +0100

    logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,17 @@ import s from './Header.module.css';
 import Logo from './../../assets/logo.jpg';
 import { NavLink } from 'react-router-dom';
 
-const Header = ({isAuth, login}) => {
+const Header = ({isAuth, login, logoutUserData}) => {
     return (
         <header className={s.header}>
             <div className={s.headerLogo}>
                 <a href='#'><img src={Logo} /></a>
             </div>
             <div className={s.loginBlock}>
-                {isAuth? login : <NavLink to={'/login'}>Login</NavLink>}
+                {isAuth
+                ? 
+                <div>{login} <button onClick={logoutUserData}>Log out</button></div> 
+                : <NavLink to={'/login'}>Login</NavLink>}
             </div>
         </header>
     );
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Header from './Header';
-import { getAuthUserData } from './../../redux/authReducer';
+import { getAuthUserData, logoutUserData } from './../../redux/authReducer';
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
@@ -20,5 +20,5 @@ const mapStateToProps = (state) => ({
     login: state.auth.login
 });
 
-export default connect(mapStateToProps, {getAuthUserData})(HeaderContainer);
+export default connect(mapStateToProps, {getAuthUserData, logoutUserData})(HeaderContainer);
 
